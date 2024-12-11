42168821a1e5fea251a10ffb37a143f7ae1ffb88
Emerson Leite <emerson.bl@gmail.com>
2024-12-11 18:33:50 -0300

    chore: update Resident enetity

--- src/residents/entities/resident.entity.ts
+++ src/residents/entities/resident.entity.ts
@@ -1,6 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Resident {
-  id: number
-  name: string
-  apartment: string
-  building: string // Bloco
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  apartment: string;
+
+  @Column({ type: 'varchar' })
+  building: string; // Bloco do condomínio
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
