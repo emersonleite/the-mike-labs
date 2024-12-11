5fec562349b5e71562dccd15ee6caa366d737565
Emerson Leite <emerson.bl@gmail.com>
2024-12-11 18:31:53 -0300

    chore: update residents module

--- src/residents/residents.module.ts
+++ src/residents/residents.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ResidentsService } from './residents.service';
 import { ResidentsController } from './residents.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Resident } from './entities/resident.entity';
 
 /**
  * Módulo de moradores (ResidentsModule).
@@ -8,6 +10,7 @@ import { ResidentsController } from './residents.controller';
  * Ele registra o serviço e o controlador relacionados aos moradores.
  */
 @Module({
+  imports: [TypeOrmModule.forFeature([Resident])], // Importa as entidades necessárias para o módulo
   controllers: [ResidentsController], // Define os controladores do módulo
   providers: [ResidentsService], // Define os provedores (services) do módulo
 })
