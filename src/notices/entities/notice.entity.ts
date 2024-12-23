f559f1707df5aaf64a3e4d80f4c948223f663404
Emerson Leite <emerson.bl@gmail.com>
2024-12-23 15:37:21 -0300

    chore: update notices resources

--- src/notices/entities/notice.entity.ts
+++ src/notices/entities/notice.entity.ts
@@ -1,1 +1,33 @@
-export class Notice {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Notice {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  title: string;
+
+  @Column({ type: 'varchar', length: 250 })
+  description: string;
+
+  // Adaptar para ENUM
+  @Column({ type: 'varchar' })
+  priority: string;
+
+  // Adaptar para ENUM
+  @Column({ type: 'varchar' })
+  status: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
