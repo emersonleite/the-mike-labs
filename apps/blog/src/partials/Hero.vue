a65683e3a3c0c542275674cedd581e8ee0e2461d
Emerson Leite <emerson.bl@gmail.com>
2025-06-08 20:34:47 -0300

    chore(blog): change Hero partial

--- apps/blog/src/partials/Hero.vue
+++ apps/blog/src/partials/Hero.vue
@@ -2,21 +2,26 @@
   <Section>
     <HeroAvatar>
       <template #title> Olá, eu sou o <GradientText>Emerson</GradientText> 👋 </template>
-      <template #description
-        >Nesse misto de site e blog vou apresentar meus projetos e pensamentos. Ele é especimente dedicado ao meu eterno
-        cãozinho <a href="" class="text-cyan-400 hover:underline">Mike Tyson</a> (in memoriam).
-      </template>
 
-      <template #avatar>
-        <img class="h-64 w-64" :src="'/assets/images/social/mike.png'" alt="Avatar image" loading="lazy" />
-      </template>
       <template v-slot:socialButtons>
-        <a href="/">
+        <a class="mr-2" href="/">
           <HeroSocial :src="'/assets/images/social/linkedin-icon.png'" :alt="'Linkedin icon'" />
         </a>
         <a href="/">
           <HeroSocial :src="'/assets/images/social/github-icon.png'" :alt="'Github icon'" />
         </a>
+      </template>
+
+      <template #description
+        >O objetivo desse blog é apresentar meus projetos e pensamentos. Pretendo falar sobre tecnologia, como ela pode
+        facilitar nossa vida entre outros aspectos. Minha intenção é, também, abordar outros assuntos, apresentados de
+        forma natural e que mostrem um pouco sobre como penso e vivo (não, necessariamente nesse ordem). Esse blog é
+        especialmente dedicado ao meu eterno cãozinho <a href="" class="text-cyan-400 hover:underline">Mike</a> (in
+        memoriam) que dá nome a ele (blog), representado, alegremente, ao lado.
+      </template>
+
+      <template #avatar>
+        <img class="h-64 w-64" :src="'/assets/images/social/mike.png'" alt="Avatar image" loading="lazy" />
       </template>
     </HeroAvatar>
   </Section>
