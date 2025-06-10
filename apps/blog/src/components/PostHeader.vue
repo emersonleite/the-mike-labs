b4a68a8461a4524a3bb466292148f20eb1dccea8
Emerson Leite <emerson.bl@gmail.com>
2025-06-10 10:14:21 -0300

    chore: change apps names

--- apps/blog/src/components/PostHeader.vue
+++ apps/blog/src/components/PostHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-center text-3xl font-bold">{{ content.title }}</h1>
-  <div class="mt-2 text-center text-sm text-gray-400">By {{ author }} on {{ formattedDate }}</div>
+  <div class="mt-2 text-center text-sm text-gray-400">Por {{ author }} em {{ formattedDate }}</div>
 </template>
 
 <script setup>
@@ -9,7 +9,7 @@ import { computed } from 'vue';
 const props = defineProps(['content', 'author']);
 
 const formattedDate = computed(() => {
-  return new Date(props.content.pubDate).toLocaleString('en-US', {
+  return new Date(props.content.pubDate).toLocaleString('pt-BR', {
     month: 'long',
     day: 'numeric',
     year: 'numeric',
