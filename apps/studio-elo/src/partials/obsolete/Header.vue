543493bc6e7dd838a5287409e66d947c3615d932
Emerson Leite <emerson.bl@gmail.com>
2025-07-07 15:08:53 -0300

    chore: remove obsolete files

--- apps/studio-elo/src/partials/obsolete/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <header class="w-full bg-dark text-white">
-    <Header dark />
-  </header>
-</template>
-
-<script setup>
-import Header from '@/components/Header.vue';
-</script>
--- apps/studio-elo/src/partials/obsolete/HeroSection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <VueHero />
-</template>
-
-<script setup>
-import VueHero from '@/components/obsolete/VueHero.vue';
-</script>
