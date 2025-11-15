b2bbc5be91bac3ffb1d36975f5625633f0fc116e
Emerson Leite <emerson.bl@gmail.com>
2025-11-15 14:52:32 -0300

    style: standardize import statements and formatting in ResidentsService

--- src/residents/residents.service.ts
+++ src/residents/residents.service.ts
@@ -3,28 +3,28 @@ import {
   ConflictException,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { CreateResidentDto } from './dto/create-resident.dto';
-import { UpdateResidentDto } from './dto/update-resident.dto';
-import { Resident } from './entities/resident.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+} from "@nestjs/common";
+import { CreateResidentDto } from "./dto/create-resident.dto";
+import { UpdateResidentDto } from "./dto/update-resident.dto";
+import { Resident } from "./entities/resident.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class ResidentsService {
   constructor(
     @InjectRepository(Resident) // Injetando o repositório a partir de Resident (entidade)
-    private readonly residentRepository: Repository<Resident>,
+    private readonly residentRepository: Repository<Resident>
   ) {}
 
   async findAll(): Promise<Resident[]> {
-    return await this.residentRepository.find({ relations: ['notices'] });
+    return await this.residentRepository.find({ relations: ["notices"] });
   }
 
   async findOne(id: string): Promise<Resident> {
     const resident = await this.residentRepository.findOne({
       where: { id },
-      relations: ['notices'],
+      relations: ["notices"],
     });
     if (!resident) {
       throw new NotFoundException(`Resident with ID ${id} not found.`);
@@ -39,7 +39,7 @@ export class ResidentsService {
 
     if (isThereUserEmail) {
       throw new ConflictException(
-        `Resident with email ${residentDto.email} already exists.`,
+        `Resident with email ${residentDto.email} already exists.`
       );
     }
 
@@ -63,7 +63,7 @@ export class ResidentsService {
       /* Captura o erro, e compara o código para retorno */
       if (error.driverError.errno === 19) {
         throw new ConflictException(
-          `Resident with email ${residentDto.email} already exists.`,
+          `Resident with email ${residentDto.email} already exists.`
         );
       }
       throw error;
@@ -72,8 +72,8 @@ export class ResidentsService {
 
   async update(id: string, updateResidentDto: UpdateResidentDto) {
     /* Evitando que o email possa ser atualizado mesmo que esteja presente no DTO */
-    if ('email' in updateResidentDto) {
-      throw new BadRequestException('Email cannot be updated');
+    if ("email" in updateResidentDto) {
+      throw new BadRequestException("Email cannot be updated");
     }
 
     const resident = await this.residentRepository.preload({
@@ -95,5 +95,9 @@ export class ResidentsService {
     }
     await this.residentRepository.remove(resident);
     return resident;
+  }
+
+  async deleteAll() {
+    await this.residentRepository.delete({});
   }
 }
