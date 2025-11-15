b478d917046542e154bfeca4cfa99b0b078dd7e5
Emerson Leite <emerson.bl@gmail.com>
2025-11-15 14:52:50 -0300

    style: standardize quotes in Resident entity

--- src/residents/entities/resident.entity.ts
+++ src/residents/entities/resident.entity.ts
@@ -1,5 +1,5 @@
-import { Exclude } from 'class-transformer';
-import { Notice } from '../../notices/entities/notice.entity';
+import { Exclude } from "class-transformer";
+import { Notice } from "../../notices/entities/notice.entity";
 import {
   Column,
   CreateDateColumn,
@@ -7,30 +7,30 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-} from 'typeorm';
+} from "typeorm";
 
 @Entity()
 export class Resident {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: "varchar", length: 50 })
   name: string;
 
   @Column({ unique: true }) // Garante que o e-mail seja único no banco
   email: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: "varchar" })
   apartment: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: "varchar" })
   building: string; // Bloco do condomínio
 
   @Column()
   @Exclude() // Exclui, na resposta, o envio do campo passwordHash
   passwordHash: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: "varchar" })
   phone: string;
 
   @CreateDateColumn()
